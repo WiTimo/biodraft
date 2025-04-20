b11a3ec032f7f005dc7afbd0138af334baa4b76f
steffking <steffi01st@icloud.com>
2025-04-20 18:54:45 +0200

    Restart

--- src/lib/colors.ts
+++
@@ -1,5 +0,0 @@
-export const HANDLE_COLOR = "gray";
-export const LINE_COLOR = "blue";
-export const POINT_COLOR = "blue";
-export const PREVIEW_LINE_COLOR = "#0000ff33"
-export const POINT_OUTLINE_COLOR = "#0000ff33"
